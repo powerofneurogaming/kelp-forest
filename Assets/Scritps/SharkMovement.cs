e9902767e6d86a86c5262d1f314858c15e5b863d
Joshvu93 <joshvu93@gmail.com>
2020-06-24 08:38:50 -0700

    Added more goons and movement to each. Will need to work on creating a prefab and more dynamic movement. wip

---
+++ Assets/Scritps/SharkMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SharkMovement : MonoBehaviour {
+    public float raycastLength;
+
+    public float goombaSpeed;
+
+    public bool hitObject = false;
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * goombaSpeed * Time.deltaTime);
+
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        // if (hitObject == false Physics.Raycast(transform.position, left, raycastLength))
+        // {
+        //     print("HIT");
+        //     hitObject = false;
+        //     }
+        }
+    }
