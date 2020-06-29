eb58b4bfc4937a8bc707e7de433bdadaae15f334
Joshvu93 <joshvu93@gmail.com>
2020-06-29 09:18:19 -0700

    Added particles to simulate bubbles on hero and goons.

---
+++ Assets/Scritps/GoonMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoonMovement : MonoBehaviour {
+    public float raycastLength;
+
+    public float goonSpeed;
+
+    public bool hitObject = false;
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * goonSpeed * Time.deltaTime);
+
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        // if (hitObject == false Physics.Raycast(transform.position, left, raycastLength))
+        // {
+        //     print("HIT");
+        //     hitObject = false;
+        //     }
+        }
+    }
