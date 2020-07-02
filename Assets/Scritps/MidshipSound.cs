6b37f04558fb6bb8b81f814d3af3abdc2d492da9
Joshvu93 <joshvu93@gmail.com>
2020-07-02 08:44:12 -0700

    implemented a button feature for midshipman. The button will play the character's sound when pressed.

---
+++ Assets/Scritps/MidshipSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MidshipSound : MonoBehaviour
+{
+  public AudioSource someSound;
+  void Start () {
+      
+  }
+  
+  void Update () {
+         if (Input.GetKeyDown(KeyCode.X)) {
+             if(!someSound.isPlaying) {
+                 someSound.Play();
+             }
+         }
+      
+         if (Input.GetKeyUp(KeyCode.X)) {
+             someSound.Stop();
+         }
+     }
+ }
