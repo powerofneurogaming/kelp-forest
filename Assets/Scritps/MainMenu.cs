cd437bdf67aab63b9ef3bc8927a83c401d33b916
BlueOcean3 <torumman@ucsd.edu>
2020-07-30 10:45:13 -0700

    Revert "Removed Library and Temp folder, and changes to menu, added kelp forest level"
    This reverts commit e8e76c419f737e249bd8a7e1fb3534b09fd0859d.

--- Assets/Scritps/MainMenu.cs
+++ Assets/Scritps/MainMenu.cs
@@ -5,10 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    //public Animator transition;
-    //public float transitionTime = 1f;
-    //public GameObject Playbutton;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -18,25 +14,13 @@ public class MainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        //    PlayGame();
-        //}
+        
     }
 
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);     //This transitions to next scene.
     }
-
-    //IEnumerator LoadLevel(int levelIndex)
-    //{
-    //    transition.SetTrigger("Start"); //Play animation
-
-    //    yield return new WaitForSeconds(transitionTime); //Wait
-
-    //    SceneManager.LoadScene(levelIndex); //Load scene
-    //}
 
     public void QuitGame ()
     {
