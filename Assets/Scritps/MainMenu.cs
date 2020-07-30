e8e76c419f737e249bd8a7e1fb3534b09fd0859d
BlueOcean3 <torumman@ucsd.edu>
2020-07-30 09:30:34 -0700

    Removed Library and Temp folder, and changes to menu, added kelp forest level
    Removed Library and Temp folder.  Added music to menu, kelp forest level. Created level transition, kelp forest level. Moved Kelp Forest level to scenes.

--- Assets/Scritps/MainMenu.cs
+++ Assets/Scritps/MainMenu.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    //public Animator transition;
+    //public float transitionTime = 1f;
+    //public GameObject Playbutton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,13 +18,25 @@ public class MainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    PlayGame();
+        //}
     }
 
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);     //This transitions to next scene.
     }
+
+    //IEnumerator LoadLevel(int levelIndex)
+    //{
+    //    transition.SetTrigger("Start"); //Play animation
+
+    //    yield return new WaitForSeconds(transitionTime); //Wait
+
+    //    SceneManager.LoadScene(levelIndex); //Load scene
+    //}
 
     public void QuitGame ()
     {
