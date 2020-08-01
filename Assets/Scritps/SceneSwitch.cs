93832c3a503078f156c40f3e8f2b9c8a938c951a
BlueOcean3 <torumman@ucsd.edu>
2020-08-01 12:11:08 -0700

    Added in Scene Switcher Trigger
    When colliding with this sprite, it transitions to the next level. Just add a level after it in Build Settings.

---
+++ Assets/Scritps/SceneSwitch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    [SerializeField] private string newLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);     //This transitions to next scene.
+        }
+    }
+}
