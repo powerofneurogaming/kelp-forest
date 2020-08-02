137d7e9ee91b76a4e91f78674735e850f76e91a5
Ara Jung <a4jung@ucsd.edu>
2020-08-01 17:57:14 -0700

    Scene and Asset Fixes
    Score text changed and placed in the right location, fish and rock assets fixed, audio added in seagrass level, crabs collider replaced with poly, sharks set as jellyfish parent for seagrass.

---
+++ Assets/Scripts/CrabMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrabMovement : MonoBehaviour {
+    public float goonSpeed;
+    public bool hitObject = true;
+    public int coinValue = 2;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            ScoreManager.instance.ChangeScore(coinValue);
+        }
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * goonSpeed * Time.deltaTime);
+
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        }
+    }
---
+++ Assets/Scripts/GoonMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoonMovement : MonoBehaviour {
+    public float raycastLength;
+
+    public float goonSpeed;
+
+    public bool hitObject = false;
+    public Rigidbody2D rigid;
+
+    void Start ()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * goonSpeed * Time.deltaTime);
+
+        Vector3 right = transform.TransformDirection(Vector3.right);
+    }
+
+    void OnCollisionEnter2D (Collision2D coll)
+    {
+        if (coll.gameObject.name.Contains("Projectile"))
+        {
+            Destroy(coll.gameObject);
+        }
+    }
+}
